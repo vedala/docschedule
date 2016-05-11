48cace533fb8b80354c255eee8e62178771a2897
Sundaram Vedala <svedala23@yahoo.com>
2016-05-11 04:03:04 -0400

    Modify line endings

--- src/main/java/com/docschedule/domain/DailySchedule.java
+++ src/main/java/com/docschedule/domain/DailySchedule.java
@@ -1,1 +1,26 @@
-package com.coderapids.docsched.domain;import java.util.ArrayList;public class DailySchedule {   private String scheduleDate;   private ArrayList<PhysicianInfo> physicianInfoArray;   	public String getScheduleDate() {		return scheduleDate;	}		public void setScheduleDate(String scheduleDate) {		this.scheduleDate = scheduleDate;	}	public ArrayList<PhysicianInfo> getPhysicianInfoArray() {		return physicianInfoArray;	}		public void setPhysicianInfoArray(ArrayList<PhysicianInfo> physicianInfoArray) {		this.physicianInfoArray = physicianInfoArray;	}}
+package com.coderapids.docsched.domain;
+
+import java.util.ArrayList;
+
+public class DailySchedule {
+   private String scheduleDate;
+   private ArrayList<PhysicianInfo> physicianInfoArray;
+   
+
+	public String getScheduleDate() {
+		return scheduleDate;
+	}
+	
+	public void setScheduleDate(String scheduleDate) {
+		this.scheduleDate = scheduleDate;
+	}
+
+	public ArrayList<PhysicianInfo> getPhysicianInfoArray() {
+		return physicianInfoArray;
+	}
+	
+	public void setPhysicianInfoArray(ArrayList<PhysicianInfo> physicianInfoArray) {
+		this.physicianInfoArray = physicianInfoArray;
+	}
+}
+
