bd159947c45a46dac3cfcd5aeb1618744e112f7a
Sundaram Vedala <svedala23@yahoo.com>
2016-05-26 13:58:26 -0400

    Create signup servlet

---
+++ src/main/java/com/docschedule/servlet/SignupUser.java
@@ -0,0 +1,17 @@
+
+package com.docschedule.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.HttpServlet;
+import javax.Servlet.ServletRequest;
+import javax.Servlet.ServletResponse;
+import javax.Servlet.ServletException;
+
+
+public class SignupUser extends HttpServlet {
+
+    public void service(ServletRequest request, ServletResponse response)
+                                    throws ServletException, IOException {
+    }
+}
