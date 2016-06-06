2d390027946c085032eafe7ef2aba81069eb5a69
Sundaram Vedala <svedala23@yahoo.com>
2016-06-06 13:55:36 -0400

    Add basic signup functionality

--- src/main/java/com/docschedule/servlet/SignupUser.java
+++ src/main/java/com/docschedule/servlet/SignupUser.java
@@ -2,16 +2,69 @@
 package com.docschedule.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.HttpServlet;
-import javax.Servlet.ServletRequest;
-import javax.Servlet.ServletResponse;
-import javax.Servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import java.nio.charset.StandardCharsets;
 
 public class SignupUser extends HttpServlet {
 
-    public void service(ServletRequest request, ServletResponse response)
-                                    throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+                                    throws IOException {
+        Connection connection = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        DataSource dataSource = null;
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            Context context = new InitialContext();
+            dataSource = (DataSource) context.lookup("java:comp/env/jdbc/docschedDB");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = dataSource.getConnection();
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte hashedBytes[] = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            String passwordHash = Hex.encodeHexString(hashedBytes);
+
+            String sqlString = "insert into users (username, password) values (?, ?)";
+
+            preparedStatement = connection.prepareStatement(sqlString);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, passwordHash);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-256 is not available", e);
+        }
     }
 }
