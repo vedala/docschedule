47426f49e21e9f95b50f7a84e4a2338040d65a43
Sundaram Vedala <svedala23@yahoo.com>
2016-06-16 16:21:25 -0400

    Create html and servlet files for schedule creation

---
+++ src/main/java/com/docschedule/servlet/CreateSched.java
@@ -0,0 +1,16 @@
+
+package com.docschedule.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CreateSched extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+                                    throws IOException {
+
+    }
+}
