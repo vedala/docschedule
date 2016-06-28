efa78e1713ddbdf61ddae00e5cbded0dd7bd1946
Sundaram Vedala <svedala23@yahoo.com>
2016-06-28 10:33:20 -0400

    Schedule still displayed if navigating out and back into page

--- src/main/java/com/docschedule/controller/ViewScheduleController.java
+++ src/main/java/com/docschedule/controller/ViewScheduleController.java
@@ -4,6 +4,7 @@ package com.docschedule.controller;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
@@ -20,18 +21,42 @@ public class ViewScheduleController extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
                                     throws IOException, ServletException {
 
-        request.getRequestDispatcher("schedmain_new.html").forward(request, response);
+        HttpSession session = request.getSession();
+
+        // if startDate attribute exists in session, then display results by
+        // calling doPost(), otherwise just navigate to display page
+    
+        String startDate = (String) session.getAttribute("startDate");
+        request.setAttribute("fromGet", "Y");
+
+        if (startDate == null) {
+            request.getRequestDispatcher("schedmain_new.html").forward(request, response);
+        }
+        else {
+            doPost(request, response);
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
                                     throws IOException, ServletException {
 
-        String startDate = request.getParameter("formdate");
+        String startDate = null;
+        HttpSession session = request.getSession();
 
-        int daysToDisplay = 7;
+        String isFromGet = (String) request.getAttribute("fromGet");
+
+        if (isFromGet == null) {
+            startDate = request.getParameter("formdate");
+            session.setAttribute("startDate", startDate);
+        }
+        else {
+            startDate = (String) session.getAttribute("startDate");
+        }
+
+        int daysToDisplay = 10;
 
         LocalDate sDate = LocalDate.parse(startDate);
-        LocalDate eDate = sDate.plusDays(daysToDisplay);
+        LocalDate eDate = sDate.plusDays(daysToDisplay-1);
         String endDate = eDate.toString();
 
         List<DailySchedule> scheduleArr = ViewSchedule.getSchedule(startDate, endDate);
