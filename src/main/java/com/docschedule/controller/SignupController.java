de7cacee5fce103adc7589f68897052493cda3c6
Sundaram Vedala <svedala23@yahoo.com>
2016-06-30 10:44:28 -0400

    Signup controller, validate all inputs, display multiple error messages

--- src/main/java/com/docschedule/controller/SignupController.java
+++ src/main/java/com/docschedule/controller/SignupController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 import com.docschedule.model.service.SignupUser;
 
@@ -17,11 +19,35 @@ public class SignupController extends HttpServlet {
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        String cofirm = request.getParameter("confirm");
+        String confirm = request.getParameter("confirm");
         String toEmail = request.getParameter("email");
 
+        List<String> errorMessage = new ArrayList<String>();
+
+        if (username == null || username.equals("")) {
+            errorMessage.add("Username must be entered.");
+        }
+
+        if (password == null || password.equals("")) {
+            errorMessage.add("Password must be entered.");
+        }
+
+        if (confirm == null || confirm.equals("")) {
+            errorMessage.add("Password confirm must be entered.");
+        }
+
+        if (password != null && confirm != null) {
+            if (!password.equals(confirm)) {
+                errorMessage.add("Password and confirm must be match.");
+            }
+        }
+
         if (toEmail == null || toEmail.equals("")) {
-            request.getSession().setAttribute("signupErrorMessage", "Email must be entered");
+            errorMessage.add("Email must be entered.");
+        }
+
+        if (errorMessage.size() > 0) {
+            request.getSession().setAttribute("signupErrorMessage", errorMessage);
             request.getRequestDispatcher("signup.html").forward(request, response);
         }
         else {
