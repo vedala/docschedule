d04589edbe764abc55c4555f202dcf7e4db0562b
Sundaram Vedala <svedala23@yahoo.com>
2016-07-08 14:10:16 -0400

    Perform additional validations for create schedule page

--- src/main/java/com/docschedule/controller/CreateScheduleController.java
+++ src/main/java/com/docschedule/controller/CreateScheduleController.java
@@ -9,12 +9,14 @@ import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.sql.Date;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.time.DayOfWeek;
 
 import com.docschedule.model.service.CreateSchedule;
+import com.docschedule.model.dao.ScheduleDAO;
 
 public class CreateScheduleController extends HttpServlet {
 
@@ -63,13 +65,46 @@ public class CreateScheduleController extends HttpServlet {
             }
         }
 
+        // Display errors to user if any of the above validations fail
+
         if (errorMessage.size() > 0) {
             return;
         }
 
         LocalDate sDate = LocalDate.parse(startDate);
+        LocalDate eDate = LocalDate.parse(endDate);
+
+        if (!eDate.isAfter(sDate)) {
+            errorMessage.add("End date must be higher than start date");
+            return;
+        }
+
+        // check if schedule already exists, if exists then perform additional checks
+        // otherwise any range of dates is allowed
+
+        ScheduleDAO scheduleDAO = new ScheduleDAO();
+        int count = scheduleDAO.getScheduleCount();
+        if (count > 0) {
+            // check if schedule already exists for date range specified,
+            // display highest date for which schedule is available
+
+            int rangeCount = scheduleDAO.checkScheduleInRange(startDate, endDate);
+            if (rangeCount > 0) {
+                Date maxDate = scheduleDAO.getMaxScheduleDate();
+                String err = "Schedule exists for all or part of range provided";
+                if (maxDate != null) {
+                    err = err + " (schedule exists till " + maxDate.toString() + ")";
+                }
+                errorMessage.add(err);
+            }
+        }
+
         if (sDate.getDayOfWeek() != DayOfWeek.TUESDAY) {
             errorMessage.add("Start date must be a Tuesday");
+        }
+
+        if (eDate.getDayOfWeek() != DayOfWeek.MONDAY) {
+            errorMessage.add("End date must be a Monday");
         }
     }
 }
--- src/main/java/com/docschedule/model/dao/ScheduleDAO.java
+++ src/main/java/com/docschedule/model/dao/ScheduleDAO.java
@@ -10,6 +10,7 @@ import javax.naming.NamingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.sql.Date;
 
 
@@ -43,5 +44,105 @@ public class ScheduleDAO {
                 System.out.println("SQLException: " + e.getMessage());
             }
         }
+    }
+
+    public int getScheduleCount() {
+
+        Connection connection = null;
+        DataSource ds = AppDataSource.getDataSource();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int count = -1;
+
+        try {
+            connection = ds.getConnection();
+
+            String sqlString =   "select count(*) from schedule";
+
+            preparedStatement = connection.prepareStatement(sqlString);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            count = resultSet.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("SQL Exception-close");
+                System.out.println("SQLException: " + e.getMessage());
+            }
+        }
+
+        return count;
+    }
+
+    public int checkScheduleInRange(String startDate, String endDate) {
+
+        Connection connection = null;
+        DataSource ds = AppDataSource.getDataSource();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        int count = -1;
+
+        try {
+            connection = ds.getConnection();
+
+            String sqlString =   "select count(*) from schedule where "
+                                            + "schedule_date between ? and ?";
+
+            preparedStatement = connection.prepareStatement(sqlString);
+            preparedStatement.setString(1, startDate);
+            preparedStatement.setString(2, endDate);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            count = resultSet.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("SQL Exception-close");
+                System.out.println("SQLException: " + e.getMessage());
+            }
+        }
+
+        return count;
+    }
+
+    public Date getMaxScheduleDate() {
+
+        Connection connection = null;
+        DataSource ds = AppDataSource.getDataSource();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Date maxDate = null;
+
+        try {
+            connection = ds.getConnection();
+
+            String sqlString =   "select max(schedule_date) from schedule";
+
+            preparedStatement = connection.prepareStatement(sqlString);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            maxDate = resultSet.getDate(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("SQL Exception-close");
+                System.out.println("SQLException: " + e.getMessage());
+            }
+        }
+
+        return maxDate;
+
     }
 }
